841dd53e7731c4f6df694a6914def370131dcaf2
Areesha-sites <areesha21314@gmail.com>
2024-11-13 15:01:20 +0500

    something changed

--- src/app/components/TextContent.tsx
+++ src/app/components/TextContent.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-const TextContent = ({ children}) => {
+
+interface TextContentProps {
+  children: React.ReactNode;
+}
+
+const TextContent: React.FC<TextContentProps> = ({ children }) => {
   return (
     <div className="flex flex-col justify-center lg:w-[70%] w-[80%] mx-auto">
       <div className="text-foreground font-inter dark:text-white lg:text-[15px] mt-3 text-[13px] xl:text-[17px] 2xl:text-[21px]">
         {children}
-     
       </div>
-    </div> 
-  )
+    </div>
+  );
 };
 
 export default TextContent;
